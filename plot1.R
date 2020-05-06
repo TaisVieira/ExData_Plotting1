8dbe4d1ec63bc903275ca1b41fd8358737513c97
TaisVieira <63369712+TaisVieira@users.noreply.github.com>
2020-05-06 14:01:49 -0300

    Delete plot1.R

--- plot1.R
+++
@@ -1,5 +0,0 @@
-data<-read.table("household_power_consumption.txt", sep=";", header=T, na.strings = "?")
-data<-data[(data$Date=="1/2/2007" | data$Date=="2/2/2007"),]
-
-with(data, hist(Global_active_power, col="red", xlab="Global active power (kilowatts)", main="Global active power"))
-
